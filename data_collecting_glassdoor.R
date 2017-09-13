3efe634d1915bf7c73f191bb70ebe819d452878f
bingkaiwang <bingkai.w@gmail.com>
2017-09-13 16:50:05 -0400

    Finish the code for glassdoor scraping

--- data_collecting_glassdoor.R
+++ data_collecting_glassdoor.R
@@ -1,29 +1,40 @@
 # collecting data for glassdoor
 require(rvest)
+ptm <- proc.time()
 currentpage <- html_session("https://www.glassdoor.com/Job/jobs.htm?suggestCount=0&suggestChosen=true&clickSource=searchBtn&typedKeyword=data+sc&sc.keyword=data+scientist&locT=&locId=&jobType=")
-company <- NULL
+company <- rep(NA, 10000)
 # salary <- NULL
-location <- NULL
-description <- NULL
-for(i in 1:5){
+location <- rep(NA, 10000)
+description <- rep(NA, 10000)
+count <- 0
+for(i in 1:3){
   # company <- c(company, currentpage %>% html_nodes(".empLoc div") %>% html_text())
   # salary <- c(salary, currentpage %>% html_nodes(".small") %>% html_text())
   # location <- c(location, currentpage %>% html_nodes(".loc") %>% html_text())
-  link <- currentpage %>% html_nodes(".jobLink") %>% html_attr("href")
+  link <- currentpage %>% html_nodes("a.jobLink") %>% html_attr("href")
   link <- paste('www.glassdoor.com', link, sep = '')
   link <- unique(link)
   for(j in link){
     subpage <- html_session(j)
     if(length(grep("glassdoor", subpage$url))){
-      company <- c(company, subpage %>% html_node(".padRtSm") %>% html_text())
-      location <- c(location, subpage %>% html_node(".subtle") %>% html_text())
-      description <- c(description, subpage %>% html_node(".desc") %>% html_text())
+      add_company <- subpage %>% html_node(".padRtSm") %>% html_text()
+      if(is.na(add_company) | add_company %in% company){
+        next
+      }else{
+        count <- count + 1
+        company[count] <- add_company
+        location[count] <- subpage %>% html_node(".subtle") %>% html_text()
+        description[count] <- subpage %>% html_node(".desc") %>% html_text()
+      }
     }
   }
-  nextpage <- glassdoor %>% html_nodes("#FooterPageNav a") %>% html_attr("href")
+  nextpage <- currentpage %>% html_nodes("#FooterPageNav a") %>% html_attr("href")
   nextpage <- paste('www.glassdoor.com', nextpage, sep = '')
   currentpage <- html_session(nextpage[length(nextpage)])
+  Sys.sleep(5)
 }
-demo_raw_data <- data.frame(company = company, location = location, 
-                            description = description)
+proc.time() - ptm
+count
+demo_raw_data <- data.frame(company = company[1:count], location = location[1:count], 
+                            description = description[1:count])
 write.csv(demo_raw_data, "demo_raw_data.csv")
